4d8ddee8341e2136a0972b6ccbbd13369c6a6008
archidote <archidote.contact@gmail.com>
2022-08-05 16:56:52 +0200

    requests lib, --> update to latest version in requirments.txt to avoid conflict with tweepy v4

--- start.py
+++ start.py
@@ -7,8 +7,8 @@ from quote_publisher import *
 from threading import Thread
 from reply import detect_twitter_mention
 
-# schedule.every().day.at("07:30").do(lambda: img_publisher())
-# schedule.every().day.at("18:00").do(lambda: quote_publisher())
+# schedule.every().days.at("07:30").do(lambda: meme_publisher())
+# schedule.every(2).days.at("18:00").do(lambda: quote_publisher())
 
 schedule.every(1).minutes.do(lambda: meme_publisher())
 schedule.every(2).minutes.do(lambda: quote_publisher())
@@ -18,8 +18,8 @@ def send_img_or_quote_auto() :
         schedule.run_pending()
         time.sleep(1)
 
-x = threading.Thread(target=detect_twitter_mention)
-x.start()
+# x = threading.Thread(target=detect_twitter_mention)
+# x.start()
 
 y = threading.Thread(target=send_img_or_quote_auto)
 y.start()
