91b27dfcda72a56ff7af9ca90c8d823b0bd45321
archidote <archidote.contact@gmail.com>
2022-09-17 10:02:52 +0200

    remove python virtual env statement in main.yml. (to complex at this moment to manage with screen cmd)

--- assets/old/reddit_api_auth.old.py
+++ assets/old/reddit_api_auth.old.py
@@ -1,9 +1,9 @@
-import praw
-r = praw.Reddit(client_id='',
-                client_secret='',
-                user_agent='Linux')
+# import praw
+# r = praw.Reddit(client_id='',
+#                 client_secret='',
+#                 user_agent='Linux')
 
-page = r.subreddit('ProgrammerHumor')
-top_posts = page.hot(limit=None)
-for post in top_posts:
-    print(post.url)
+# page = r.subreddit('ProgrammerHumor')
+# top_posts = page.hot(limit=None)
+# for post in top_posts:
+#     print(post.url)
