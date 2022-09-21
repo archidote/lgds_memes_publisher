5b0116f5179584f7ac7a301573e8ffa1fa93dd69
archidote <archidote.contact@gmail.com>
2022-09-21 17:27:20 +0200

    workarround v2 for twitter over capacity issue. error code 130

--- assets/bot_reply.py
+++ assets/bot_reply.py
@@ -11,15 +11,26 @@ def detect_twitter_mention () :
                 ["/info", "Hello 😊 @{} Pour toutes informations, envois nous un MP (message privé) 😊"], 
                 ["/meme", "Hello 😊 @{} voici un meme tiré au sort rien que pour toi"+bot_end_message+""]]
         
-        mentions = api.mentions_timeline() # Finding mention tweets
+
+        tries = 4
+        for trie in range(tries): 
+            try:
+                #raise Exception("Test, to see if the exception is raised") 
+                mentions = api.mentions_timeline() # Finding mention(s) tweets 
+            except Exception as exc:                        
+                if trie < tries - 1 :                      
+                    logging.warning("Received invalid answer from Twitter. (Exception : "+str(exc)+") back off for 10 minutes: ")
+                    time.sleep(60*10) # Wait 10 minutes before the other tries 
+                    continue 
+                else : 
+                    raise 
 
         i = 0 
-        
+    
         for mention in mentions:
         
             with open('assets/last_tweet_mention_id') as f:
                 previous_tweet_id = f.readlines()
-                print (previous_tweet_id[0])
                     
             if previous_tweet_id[0] != str(mention.id) : # compare str to str. otherwise python cannot find any != between the two value 
                 if mention.in_reply_to_status_id is None and mention.author.id != bot_id:
@@ -28,28 +39,21 @@ def detect_twitter_mention () :
                         logging.info('tweet '+str(mention.id)+' was favorited')
                     for word in words : 
                         if word[0] in mention.text :
-                            tries = 3
-                            for trie in range(tries):
-                                try:
-                                    if word[0] == "/meme" : 
-                                        message = word[1]
-                                        meme_source_selectore_and_publish()
-                                        media = api.media_upload("assets/tmp_local_meme.JPG") 
-                                        api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str, media_ids=[media.media_id])
-                                        logging.info('meme has been fetched and published in the reply of the following tweet : '+str(mention.id))
-                                        break
-                                    else : # /info /article 
-                                        message = word[1]
-                                        api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str)
-                                        logging.info('action '+word[0]+' was published in the reply of the following tweet : '+str(mention.id))
-                                        break
-                                except Exception as exc:
-                                    if trie < tries - 1 :
-                                        logging.warning('Received invalid answer from Twitter. ({exc}) back off for 10 minutes: ')
-                                        time.sleep(10*60) # Wait 10 minutes before the other tries 
-                                        continue 
-                                    else : 
-                                        raise 
+                            try:
+                                if word[0] == "/meme" : 
+                                    message = word[1]
+                                    meme_source_selectore_and_publish()
+                                    media = api.media_upload("assets/tmp_local_meme.JPG") 
+                                    api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str, media_ids=[media.media_id])
+                                    logging.info('meme has been fetched and published in the reply of the following tweet : '+str(mention.id))
+                                    break
+                                else : # /info /article 
+                                    message = word[1]
+                                    api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str)
+                                    logging.info('action '+word[0]+' was published in the reply of the following tweet : '+str(mention.id))
+                                    break
+                            except Exception as exc:
+                                print (exc)
         
                     with open('assets/last_tweet_mention_id', 'w') as f:
                         f.write(str(mention.id))
